bcc333d3f0d55a809675b3ce4a6acd8c9b52cf68
Nidhi-Pavuluri <nandhinidhi97@gmail.com>
2019-01-18 11:45:20 +0530

    First commit with pages

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,62 +1,21 @@
 import React from "react";
-import Table from "./table/index";
-import View from "./view/index";
-import Edit from "./edit/index";
+//import App from "./Components/App";
+import HomeSearch from "./homeSearch";
+import Pages from "./pages";
 
 import{
    BrowserRouter as Router,
    Route,
    Switch,
    Redirect
-} from 'react-router-dom';
-//import Incrementer from "./incrementer/index";
+} from "react-router-dom";
 
-const tableValues = [
-   ['101','Tony Stark','Iron Man','Avengers'],
-   ['102','Peter Parker','Spider Man','Avengers'],
-   ['103','Bruce Mayne','Bat Man','Justice League']
-];
-
-const tableHeaders = ['Id','Name','Alias','Team'];
 
 class App extends React.Component{
 
-   state = {
-      selectedId: -1,
-      selectedRecord:{}
-   }
+   
    render(){
-      return (
-         <Router>
-            <Switch>
-               <Route exact path = "/list" render ={(props) => {
-                  return <Table 
-                           values = {tableValues} 
-                           headers = {tableHeaders} 
-                           history = {props.history}
-                           />
-               }}/>
-               <Route exact path = "/view/:id" render ={(props) => {
-                  console.log(props)
-                  const data = tableValues.find(val => val[0] === props.match.params.id )
-                  const newRecord = {
-                  name:data[1],
-                  alias:data[2],
-                  team:data[3]
-                }
-                   return <View 
-                  // name = {this.state.selectedRecord.name} 
-                  // alias = {this.state.selectedRecord.alias} 
-                  // team = {this.state.selectedRecord.team}
-                  selectedRecord = {newRecord} />
-               }}/>
-               <Route exact path = "/edit" render ={() => {
-                  return <Edit/>
-               }}/>
-               <Redirect to = "/list" />
-            </Switch> 
-         </Router>
-         );
+      <Route exact path = "/search" component={Pages}/>
    }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./components/app";
+import App from "./Components/App";
 
 const render = Component => ReactDOM.render(
     <App/>,
